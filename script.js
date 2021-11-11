4101df1f9cc2e725d01afe4dca1856496d8d95ba
lawric1 <67882089+lawric1@users.noreply.github.com>
2021-11-10 22:22:40 -0300

    .

--- script.js
+++ script.js
@@ -5,26 +5,39 @@ selectionRect = getRange.getBoundingClientRect();
 top = selectionRect.top;
 left = selectionRect.left;
 
-var popup = document.createElement("div"); 
-document.documentElement.appendChild(popup);
-
-popup.style.cssText = `
+function createPopup() {
+    var popup = document.createElement("div");
+    popup.style.cssText = `
     position: absolute;
     width: 300px;
-    height: 300px;
-    padding-top: 10px;
-    padding-bottom: 10px;
-    background: white;
-    text-align: center;
+    padding: 16px;
+    background: lightblue;
     color: white;
     border-radius: 8px;
     z-index: 9999;
 
     transform: translateX(-50%);
-`;
+    `;
+    popup.style.top = (selectionRect.top + 80) + 'px';
+    popup.style.left = (selectionRect.left + (selectionRect.width * 0.5)) + 'px';
 
-popup.style.top = (selectionRect.top + 30) + 'px';
-popup.style.left = (selectionRect.left + (selectionRect.width * 0.5)) + 'px'
+    var word = document.createElement("h2");
+    word.innerText = selection.toString();
+    word.style.margin = '8px';
+    popup.appendChild(word);
+
+    for(let i = 0; i < 10; i++) {
+        var p = document.createElement("p");
+        p.innerText = 'test';
+        popup.appendChild(p);
+    }
+
+
+    document.documentElement.appendChild(popup);
+}
+
+
+createPopup();
 
 
 alert(selectionRect.top);
